b9c2e676cee5c180cb4e6f618ee0faaa7ce195ea
kimyerimweb <kimyerimweb@gmail.com>
2021-12-02 01:56:59 +0900

    ♻️ apply redux action func and store variable

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -4,8 +4,8 @@ import { Form, Input, Button } from 'antd'
 import Link from 'next/link'
 import styled from 'styled-components'
 import useInput from '../hooks/useInput'
-import { useDispatch } from 'react-redux'
-import { loginAction } from '../reducers/user'
+import { useDispatch, useSelector } from 'react-redux'
+import { logInRequestAction } from '../reducers/user'
 
 const ButtonWrapper = styled.div`
   margin-top: 10px;
@@ -17,20 +17,26 @@ const FormWrapper = styled(Form)`
 
 const LoginForm = () => {
   const dispatch = useDispatch()
-  const [id, onChangeId] = useInput('')
+  const [email, onChangeEmail] = useInput('')
   const [password, onChangePassword] = useInput('')
+  const { logInLoading } = useSelector((state) => state.user)
 
   const onSubmitForm = useCallback(() => {
-    console.log(id, password)
-    dispatch(loginAction({ id, password }))
-  }, [id, password])
+    console.log(email, password)
+    dispatch(logInRequestAction({ email, password }))
+  }, [email, password])
 
   return (
     <FormWrapper onFinish={onSubmitForm}>
       <div>
-        <label htmlFor="user-id">아이디</label>
+        <label htmlFor="user-email">아이디</label>
         <br />
-        <Input name="user-id" value={id} onChange={onChangeId} required />
+        <Input
+          name="user-email"
+          value={email}
+          onChange={onChangeEmail}
+          required
+        />
       </div>
       <div>
         <label htmlFor="user-password">비밀번호</label>
@@ -43,7 +49,7 @@ const LoginForm = () => {
         />
       </div>
       <ButtonWrapper>
-        <Button type="primary" htmlType="submit" loading={false}>
+        <Button type="primary" htmlType="submit" loading={logInLoading}>
           로그인
         </Button>
         <Link href="/signup">
--- components/UserProfile.jsx
+++ components/UserProfile.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Avatar, Card, Button } from 'antd'
 import { useCallback } from 'react'
-import { useDispatch } from 'react-redux'
-import { logoutAction } from '../reducers/user'
+import { useDispatch, useSelector } from 'react-redux'
+import { logOutRequestAction } from '../reducers/user'
 
 const UserProfile = () => {
+  const { me, logOutLoading } = useSelector((state) => state.user)
   const dispatch = useDispatch()
   const onLogOut = useCallback(() => {
-    dispatch(logoutAction())
+    dispatch(logOutRequestAction())
   }, [])
 
   return (
@@ -15,20 +16,25 @@ const UserProfile = () => {
       actions={[
         <div key="twit">
           짹짹
-          <br />0
+          <br />
+          {me.Posts.length}
         </div>,
         <div key="followings">
           팔로잉
-          <br />0
+          <br />
+          {me.follwings.length}
         </div>,
         <div key="followers">
           팔로워
-          <br />0
+          <br />
+          {me.follwers.length}
         </div>,
       ]}
     >
-      <Card.Meta avatar={<Avatar>ZC</Avatar>} title="ZeroCho" />
-      <Button onClick={onLogOut}>로그아웃</Button>
+      <Card.Meta avatar={<Avatar>{me.nickanme}</Avatar>} title={me.nickname} />
+      <Button onClick={onLogOut} loading={logOutLoading}>
+        로그아웃
+      </Button>
     </Card>
   )
 }
