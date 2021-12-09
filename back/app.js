3e9c8ea0bd2ed2f6560670c6979ca56d43b82683
kimyerimweb <kimyerimweb@gmail.com>
2021-12-09 16:24:42 +0900

    🎨 connect db to backend server

--- back/app.js
+++ back/app.js
@@ -1,12 +1,16 @@
 const express = require('express')
-const postRouter = require('./routes/post')
+const db = require('./models')
 const app = express()
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log('DB 연결 성공')
+  })
+  .catch(console.error)
 
 app.get('/api', (req, res) => {
   res.send('hello express')
 })
-
-app.use('/post', postRouter)
 
 app.listen(3065, () => {
   console.log('서버 실행 중')
